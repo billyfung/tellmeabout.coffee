7bb3e35c6d1b61e98bac34631b649d6826425203
Jin Park <jin+github@jinpark.net>
2016-01-12 15:02:47 +0900

    move models to models file and gitignore

---
+++ models.py
@@ -0,0 +1,10 @@
+from google.appengine.ext import ndb
+
+class Coffee(ndb.Model):
+    name = ndb.StringProperty()
+    roaster = ndb.StringProperty()
+    description = ndb.StringProperty()
+    tasting_notes = ndb.StringProperty()
+    date_added = ndb.DateTimeProperty(auto_now_add=True)
+    date_removed = ndb.DateTimeProperty()
+
