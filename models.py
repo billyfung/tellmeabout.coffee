94c7b49e38b152bf65f734c8d4deeb6efba44e0b
Jin Park <jin+github@jinpark.net>
2016-01-13 10:54:11 +0900

    fix merge problems

--- models.py
+++ models.py
@@ -4,7 +4,9 @@ class Coffee(ndb.Model):
     name = ndb.StringProperty()
     roaster = ndb.StringProperty()
     description = ndb.StringProperty()
-    tasting_notes = ndb.StringProperty()
     date_added = ndb.DateTimeProperty(auto_now_add=True)
     date_removed = ndb.DateTimeProperty()
-
+    price = ndb.StringProperty()
+    notes = ndb.StringProperty()
+    region = ndb.StringProperty()
+    status = ndb.StringProperty()
