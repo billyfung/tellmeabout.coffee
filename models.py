b354cdf942753cd81386a7efd1b1a43e0fb31a73
Jin Park <jin+github@jinpark.net>
2016-01-13 11:50:44 +0900

    jin is dumb

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ class Coffee(ndb.Model):
     name = ndb.StringProperty()
     roaster = ndb.StringProperty()
     description = ndb.StringProperty()
-    tasting_notes = ndb.StringProperty()
     date_added = ndb.DateTimeProperty(auto_now_add=True)
     date_removed = ndb.DateTimeProperty()
     price = ndb.StringProperty()
