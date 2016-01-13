f793fdb0bb5d7fc9be147c0194ae741b84c0ec7c
Jin Park <jin+github@jinpark.net>
2016-01-13 10:55:50 +0900

    add back tasting notes

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class Coffee(ndb.Model):
     name = ndb.StringProperty()
     roaster = ndb.StringProperty()
     description = ndb.StringProperty()
+    tasting_notes = ndb.StringProperty()
     date_added = ndb.DateTimeProperty(auto_now_add=True)
     date_removed = ndb.DateTimeProperty()
     price = ndb.StringProperty()
