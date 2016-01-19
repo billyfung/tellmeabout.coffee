6caddf5cbbe51832e9db3fbbb5882e6f06e19e62
Jin Park <jin+github@jinpark.net>
2016-01-19 23:16:43 +0900

    add victrola images and log updated and new

--- scrapers/victrola.py
+++ scrapers/victrola.py
@@ -11,11 +11,12 @@ def scrape_victrola():
     roaster = 'Victrola'
     victrola = 'http://www.victrolacoffee.com/collections/all-coffee-offerings'
     r = requests.get(victrola)
-    soup = BeautifulSoup(r.content)
+    soup = BeautifulSoup(r.content, "html.parser")
 
     coffees_for_sale = soup.find_all('a', {'class':'product-link'})
     total_coffees = len(coffees_for_sale)
     coffees_entered = 0
+    coffees_updated = 0
     error_coffees = []
     for item in coffees_for_sale:
         name,description,notes,region,status,size, product_url = [""]*7
@@ -27,6 +28,9 @@ def scrape_victrola():
         coffee_soup = BeautifulSoup(r.content)
         name = coffee_soup.h2.string
         if 'Subscription' in name:
+            total_coffees-=1
+            continue
+        if coffee_soup.find('div', {'class': 'select single'}).find('label').text != 'Size':
             total_coffees-=1
             continue
         try:
@@ -58,7 +62,9 @@ def scrape_victrola():
             except:
                 region = 'n/a'
                 pass
-        coffee_data = {'name':name, 'roaster':roaster, 'description':description, 'price':price, 'notes':notes, 'region':region, 'status':status, 'product_page':product_url, 'size':size}
+        image_url = coffee_soup.find('ul', {'class': 'bx-slider'}).find('img')['src']
+        image_content = requests.get("http:{}".format(image_url)).content
+        coffee_data = {'name':name, 'roaster':roaster, 'description':description, 'price':price, 'notes':notes, 'region':region, 'status':status, 'product_page':product_url, 'size':size, 'image': image_content}
         old_coffees = Coffee.query(Coffee.name == coffee_data['name'], Coffee.roaster==coffee_data['roaster'], Coffee.region==coffee_data['region']).fetch()
         # check if coffee already in db
         if old_coffees:
@@ -66,7 +72,11 @@ def scrape_victrola():
                 logging.warning("Query for coffee name:{}, roaster:{}, region:{} returned {} results. Results are {}".format(coffee_data['name'], coffee_data['roaster'], coffee_data['region'], len(old_coffees), old_coffees))
             for key, value in coffee_data.iteritems():
                 setattr(old_coffees[0], key,value)
-            old_coffees[0].put
+            try:
+                old_coffees[0].put()
+                coffees_updated += 1
+            except:
+                error_coffees.append(coffee_data['product_page'])
         else:
             coffee=Coffee(**coffee_data)
             try:
@@ -75,5 +85,6 @@ def scrape_victrola():
             except:
                 error_coffees.append(coffee_data['product_page'])
     # display results of scrape in logs
-    logging.info('Victrola Results:{} / {}'.format(coffees_entered, total_coffees))
+    logging.info('Victrola New Results:{} / {}'.format(coffees_entered, total_coffees))
+    logging.info('Victrola Updated Results:{} / {}'.format(coffees_updated, total_coffees))
     logging.info(error_coffees)
