dc05a2847033a0b1449504d3599ab28583440e55
Billy Fung <billy@billyfung.com>
2016-01-29 14:43:14 -0800

    scraper for 49p coffee

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -3,3 +3,4 @@ from .stumptown import scrape_stumptown
 from .victrola import scrape_victrola
 from .heart import scrape_heart
 from .bluebottle import scrape_bluebottle
+from .forty9p import scrape_49p
