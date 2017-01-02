6d27ce18789d4458a8b2b678f03ea16a1573cfd5
Billy <billy@billyfung.com>
2017-01-02 11:55:26 -0800

    fixing bluebottle (#97)
    * fixing bluebottle
    
    * yapf

--- scrapers/bluebottle.py
+++ scrapers/bluebottle.py
@@ -11,15 +11,16 @@ def scrape_bluebottle():
     roaster = 'Blue Bottle'
     bluebottle = 'https://bluebottlecoffee.com/store/coffee'
     r = requests.get(bluebottle)
+    print(r)
     soup = BeautifulSoup(r.content, "html.parser")
-    coffees_for_sale = soup.find_all('h2', {'class':'f5 lh-title man'})
+    coffees_for_sale = soup.select('h2.ma0')
     total_coffees = len(coffees_for_sale)
     coffees_entered = 0
     coffees_updated = 0
     error_coffees = []
-    ignored = ['Box', 'Kit', 'Subscriptions', 'at Home']
+    ignored = ['Box', 'Kit', 'Subscriptions', 'at Home', 'Pack']
     for item in coffees_for_sale:
-        name,description,notes,region,active,size, product_url = [""]*7
+        name, description, notes, region, active, size, product_url = [""] * 7
         price = float()
         name = item.string
         if any(word in name for word in ignored):
@@ -31,27 +32,51 @@ def scrape_bluebottle():
             r = requests.get(product_url)
             coffee_soup = BeautifulSoup(r.content, "html.parser")
             active = True
-            price = float(coffee_soup.find('span', {'class':'js-variant-price'}).string)
-            description = coffee_soup.find('p', {'class':'spec-overview'}).string
-            notes = coffee_soup.p.string.lower().split(',')
+            price = float(
+                coffee_soup.find('span', {'class': 'js-variant-price'}).string)
+            try:
+                description = coffee_soup.find(
+                    'p', {'class': 'spec-details'}).string
+            except AttributeError:
+                description = coffee_soup.find(
+                    'p', {'class': 'spec-overview'}).string
+            notes = coffee_soup.select('div.mb30')[0].string.split(',')
             # only works for not single origin
             region = country_from_name(name)
             try:
-                details = coffee_soup.find('p', {'class':'spec-details'}).contents[0].strip()
-                if country_from_name(details) != '':
+                details = coffee_soup.find(
+                    'p', {'class': 'spec-details'}).contents[0].strip()
+                if country_from_name(details) != '' and len(details) < 10:
                     region = details
             except AttributeError:
                 # if it's an espresso, then it's okay to not have region
                 if 'Espresso' in name:
                     region = ""
-            size = coffee_soup.find('select', {'id':'cart_item_model_id'}).option.string.split('Bag')[0]
+            try:
+                size = coffee_soup.find('div', {'class': 'grid-col-4'}).text
+            except AttributeError:
+                continue
             image_url = coffee_soup.img['src']
             image_content = requests.get(image_url).content
-            coffee_data = {'name': name, 'roaster': roaster, 'description': description, 'price': price, 'notes': notes, 'region': region, 'active': active, 'product_page': product_url, 'size': size, 'image': image_content}
-            coffees_updated, coffees_entered, error_coffees = add_or_update_coffee(coffee_data, coffees_updated, coffees_entered, error_coffees)
+            coffee_data = {
+                'name': name,
+                'roaster': roaster,
+                'description': description,
+                'price': price,
+                'notes': notes,
+                'region': region,
+                'active': active,
+                'product_page': product_url,
+                'size': size,
+                'image': image_content
+            }
+            coffees_updated, coffees_entered, error_coffees = add_or_update_coffee(
+                coffee_data, coffees_updated, coffees_entered, error_coffees)
 
-    logging.info('Blue Bottle New Results:{} / {}'.format(coffees_entered, total_coffees))
-    logging.info('Blue Bottle Updated Results:{} / {}'.format(coffees_updated, total_coffees))
+    logging.info('Blue Bottle New Results:{} / {}'.format(coffees_entered,
+                                                          total_coffees))
+    logging.info('Blue Bottle Updated Results:{} / {}'.format(coffees_updated,
+                                                              total_coffees))
     if error_coffees:
-        logging.warning('Blue Bottle Error coffees are: {}'.format(error_coffees))
-
+        logging.warning('Blue Bottle Error coffees are: {}'.format(
+            error_coffees))
